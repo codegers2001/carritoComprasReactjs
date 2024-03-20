2f650b2bb643045d75b1f61391b8f037e1e582d9
Lincol Eulogio <2023280030@unheval.pe>
2024-03-20 09:37:56 -0500

    creamos el evento handleclick en componente guitarra

--- src/components/Guitarra.jsx
+++ src/components/Guitarra.jsx
@@ -1,5 +1,10 @@
 export default function Guitarra({ guitarra }) {
   const { id, name, description, price, image } = guitarra;
+
+  const handleClick = () => {
+    console.log(`Agregando al carrito ${name}`);
+  };
+
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
       <div className="col-4">
@@ -13,7 +18,11 @@ export default function Guitarra({ guitarra }) {
         <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
         <p>{description}</p>
         <p className="fw-black text-primary fs-3">${price}</p>
-        <button type="button" className="btn btn-dark w-100">
+        <button
+          type="button"
+          className="btn btn-dark w-100"
+          onClick={() => handleClick(guitarra)}
+        >
           Agregar al Carrito
         </button>
       </div>
