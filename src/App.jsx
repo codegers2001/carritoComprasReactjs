8fedb4bd24ac2b44d400e26470eac19f46a05f7b
Lincol Eulogio <2023280030@unheval.pe>
2024-03-21 10:21:46 -0500

    implementamos el localstorage al carrito de compras

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Guitarra from "./components/Guitarra";
 import { db } from "./db/db";
 
 function App() {
+
+  function inicializarCarrito() {
+    const carritoGuardado = localStorage.getItem("carrito");
+    if (carritoGuardado) {
+      return JSON.parse(carritoGuardado);
+    }
+    return [];
+  }
+
   // eslint-disable-next-line no-unused-vars
   const [data, setData] = useState(db);
-  const [carrito, setCarrito] = useState([]);
+  const [carrito, setCarrito] = useState(inicializarCarrito);
 
   const maxItems = 5;
   const minItems = 1;
+
+  // obtener el carrito del localStorage
+  useEffect(() => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  }, [carrito]);
 
   function addCarrito(item) {
     // verifica si el item ya está en el carrito
     const existeItem = carrito.findIndex((guitarra) => guitarra.id === item.id);
     if (existeItem >= 0) {
-      if(carrito[existeItem].cantidad >= maxItems) return;
+      if (carrito[existeItem].cantidad >= maxItems) return;
       // si el item ya está en el carrito, aumenta la cantidad
       const updateCarrito = [...carrito];
       // aumenta la cantidad del item
